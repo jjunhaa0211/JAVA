f1f7d0c1f742531b925448e597c2ce8f5701f4a2
junha <goodjunha@gmail.com>
2022-08-03 13:49:11 +0900

    ADD:: print

---
+++ javaFoundation/src/print.java
@@ -0,0 +1,9 @@
+class PrintInt {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x + 5);
+
+        x = 7;
+        System.out.println(x + 1);
+    }
+}
