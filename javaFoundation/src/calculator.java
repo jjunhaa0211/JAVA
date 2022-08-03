a1e6b764bc4873fe8b5643e342bf2bf0f50031b7
junha <goodjunha@gmail.com>
2022-08-03 14:42:17 +0900

    ADD:: calculator + (stop)

--- javaFoundation/src/calculator.java
+++ javaFoundation/src/calculator.java
@@ -3,23 +3,70 @@ import java.util.Scanner;
 class calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("계산식을 입력해주세여! : ");
-        int x = sc.nextInt();
-        String z = "";
-        z = sc.next();
-        int y = sc.nextInt();
+        System.out.println("어떤 계산기를 원하시나요?");
+        System.out.println("1. 소수점 계산기");
+        System.out.println("2. 정수형 계산기");
+        int k = sc.nextInt();
+        if(k == 1){
+            while (true) {
+                System.out.print("실수형식을 입력해주세여! :");
+                double x = sc.nextDouble();
+                String z1 = "";
+                z1 = sc.next();
+                double y = sc.nextDouble();
 
-        if(z.equals("+")){
-            System.out.println(x + "+" + y + "=" +(x+y));
-        } else if (z.equals("-")) {
-            System.out.println(x + "-" + y + "=" +(x-y));
-        } else if (z.equals("*")) {
-            System.out.println(x + "*" + y + "=" +(x*y));
-        } else if (z.equals("/")) {
-            System.out.println(x + "/" + y + "=" +(x/y));
-        } else {
-            System.out.println("당신이 입력한 숫자는 없거나 지원하지 않습니다");
+                if (z1.equals("+")) {
+                    System.out.println(x + " + " + y + " = " + (x + y));
+                } else if (z1.equals("-")) {
+                    System.out.println(x + " - " + y + " = " + (x - y));
+                } else if (z1.equals("*")) {
+                    System.out.println(x + " * " + y + " = " + (x * y));
+                } else if (z1.equals("/")) {
+                    System.out.println(x + " / " + y + " = " + (x / y));
+                } else {
+                    System.out.println("당신이 입력한 숫자는 없거나 지원하지 않습니다");
+                }
+                stop();
+            }
+        } else if(k == 2) {
+            while (true) {
+                System.out.print("정수형식을 입력해주세여! : ");
+                int x = sc.nextInt();
+                String z2 = "";
+                z2 = sc.next();
+                int y = sc.nextInt();
+
+                if (z2.equals("+")) {
+                    System.out.println(x + " + " + y + " = " + (x + y));
+                } else if (z2.equals("-")) {
+                    System.out.println(x + " - " + y + " = " + (x - y));
+                } else if (z2.equals("*")) {
+                    System.out.println(x + " * " + y + " = " + (x * y));
+                } else if (z2.equals("/")) {
+                    System.out.println(x + " / " + y + " = " + (x / y));
+                } else {
+                    System.out.println("당신이 입력한 숫자는 없거나 지원하지 않습니다");
+                }
+                stop();
+            }
         }
+    }
 
+    private static int stop() {
+        System.out.println("'그만'을 외치면 시스템이 종료 됩니다");
+        System.out.println("'계속'을 외치면 시스템이 종료 됩니다");
+        Scanner sc = new Scanner(System.in);
+        String z3 = "";
+        z3 = sc.next();
+        if(z3.equals("그만")) {
+            System.out.println("시스템이 종료됩니다.");
+            System.exit(0);
+        } else if (z3.equals("계속")) {
+            return 0;
+        } else {
+            System.out.println("정확하게 적어주세요!!");
+            stop();
+        }
+        return 0;
     }
 }
