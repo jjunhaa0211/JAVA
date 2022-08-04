28fa470395c64e6c53dd17434df8d645e06c0ab3
junha <goodjunha@gmail.com>
2022-08-04 14:30:26 +0900

    ADD:: ageText

---
+++ javaFoundation/src/age.java
@@ -0,0 +1,55 @@
+// 문제 : 할인 대상인지 아닌지 출력해주세요.
+// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
+// 조건 : 출력예시 처럼 출력되어야 합니다.
+// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
+// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
+// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
+
+class age {
+    public static void main(String[] args) {
+        int age = 20; // 이 값을 바꿔가면서 실행해보세요.
+
+        System.out.println("당신의 나이는 " + age + "살 입니다.");
+
+        // 구현시작
+
+        //1
+        if(age <= 19 || age >= 60){
+            System.out.println("할인 대상입니다.");
+        } else {
+            System.out.println("할인 대상이 아닙니다");
+        }
+
+        //2
+        if(age <= 19){
+            if (age >= 60){
+                System.out.println("할인 대상입니다");
+            }
+        } else {
+            System.out.println("할인 대상이 아닙니다");
+        }
+
+        //3
+        if(age <= 19){
+            System.out.println("할인 대상입니다");
+        }
+        else if (age >= 60){
+            System.out.println("할인 대상입니다");
+        }
+        else {
+            System.out.println("할인 대상이 아닙니다");
+        }
+
+        //4
+        if (0 <= age && age <= 19){
+            System.out.println("할인 대상입니다");
+        } else if (age >= 65) {
+            System.out.println("할인 대상입니다");
+        } else {
+            System.out.println("할인 대상이 아닙니다");
+        }
+
+
+        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
+    }
+}
