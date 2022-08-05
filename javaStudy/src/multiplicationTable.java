c5a6eabc3e3ae957699c24b1c0e87a1e4fe5db8f
junha <goodjunha@gmail.com>
2022-08-05 14:44:28 +0900

    ADD:: multiplication table

---
+++ javaStudy/src/multiplicationTable.java
@@ -0,0 +1,89 @@
+
+class multiplicationTable {
+    public static void main(String[] args) {
+
+//        //1번
+//        System.out.println("8 * 1 = 8");
+//        System.out.println("8 * 2 = 16");
+//        System.out.println("8 * 3 = 24");
+//        System.out.println("8 * 4 = 32");
+//        System.out.println("8 * 5 = 40");
+//        System.out.println("8 * 6 = 48");
+//        System.out.println("8 * 7 = 56");
+//        System.out.println("8 * 8 = 64");
+//        System.out.println("8 * 9 = 72");
+
+//        //2번
+//        int dan = 8;
+//
+//        System.out.println(dan + "* 1 = 8");
+//        System.out.println(dan + "* 2 = 16");
+//        System.out.println(dan + "* 3 = 24");
+//        System.out.println(dan + "* 4 = 32");
+//        System.out.println(dan + "* 5 = 40");
+//        System.out.println(dan + "* 6 = 48");
+//        System.out.println(dan + "* 7 = 56");
+//        System.out.println(dan + "* 8 = 64");
+//        System.out.println(dan + "* 9 = 72");
+
+//        //문제 3
+//        int dan = 8;
+//
+//        System.out.println(dan + "1 = "+ dan * 1);
+//        System.out.println(dan + "2 = "+ dan * 2);
+//        System.out.println(dan + "3 = "+ dan * 3);
+//        System.out.println(dan + "4 = "+ dan * 4);
+//        System.out.println(dan + "5 = "+ dan * 5);
+//        System.out.println(dan + "6 = "+ dan * 6);
+//        System.out.println(dan + "7 = "+ dan * 7);
+//        System.out.println(dan + "8 = "+ dan * 8);
+//        System.out.println(dan + "9 = "+ dan * 9);
+
+//        //4번 문제
+//        int dan = 8;
+//
+//        // 수정가능지역 시작
+//        int i = 1;
+//
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+//        i++;
+//        System.out.println(dan + " * " + i + " = " + dan * i);
+
+//        //문제 5번
+//        int dan = 8;
+//
+//        for(int i = 1; i <= 1000; i++)
+//        {
+//            System.out.println(dan + " * " + i + " = " + dan * i);
+//        }
+
+
+
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
+    }
+}
