72b5653bdb95a4bbe17ec7329fd82cb5245887ea
junha <goodjunha@gmail.com>
2022-08-05 14:43:32 +0900

    ADD:: multiplication table

---
+++ javaStudy/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        String a = "안녕";
+        String b = "하세요";
+        int c = 10;
+
+        System.out.println(a + b + c);
+    }
+}
