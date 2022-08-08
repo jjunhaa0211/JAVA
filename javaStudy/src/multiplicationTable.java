72053bed425a85e1eece68bc4fddd63dc5f46f18
junha <goodjunha@gmail.com>
2022-08-08 14:18:34 +0900

    ADD:: 123 10번 출력

--- javaStudy/src/multiplicationTable.java
+++ javaStudy/src/multiplicationTable.java
@@ -92,7 +92,7 @@ class multiplicationTable {
 //    */
 //        //문제 6번 -100부터 25까지 출력
 //        int num = -100;
-//        while (num < 25){
+//        while (num <= 25){
 //            System.out.println(num);
 //            num++;
 //        }
@@ -100,10 +100,62 @@ class multiplicationTable {
         // 문제 : 단을 * 1000까지 출력해주세요.
         // 조건 : 곱하는 순서를 거꾸로 해주세요.
 //        //문제 7번
+//        int dan = 8
 //        int i = 1000;
 //        while (i != 0){
-//            System.out.println("8" + " * " + i + " = " + 8 * i);
+//            System.out.println("dan" + " * " + i + " = " + dan * i);
 //            i--;
+//        }
+        // 문제 : 아래와 같이 출력해주세요.
+        // 문제 : 단을 `* 1000`부터 `* -500`까지 출력해주세요.
+
+        //8번
+//        int dan = 8;
+//
+//        int i = 1000;
+//        while (i != -501) {
+//            System.out.println(dan + " * " + i + " + " + " = " + dan * i);
+//            i--;
+//        }
+
+        // 출력
+    /*
+    8 * 1000 = 8000
+    ...
+    8 * -500 = -4000
+    */
+        //문제9번
+//        int i = 1;
+//        int sum = 0;
+//        for(; i <= 5; i++){
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+        //문제 10번
+//        int i = -100;
+//        int sum = 0;
+//        for(; i <= 25; i++){
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+        //문제 11번
+        int cnt = 1;
+        int i = 1;
+//        while (cnt <= 10){
+//            while (i <= 3){
+//                System.out.println(i);
+//                i++;
+//            }
+//            i = 1;
+//            System.out.println(cnt + "출력");
+//            cnt++;
+//        }
+//        for(;cnt <= 10; cnt++){
+//            for(; i <= 3; i++){
+//                System.out.println(i);
+//            }
+//            i = 1;
+//            System.out.println(cnt + "출력");
 //        }
     }
 }
