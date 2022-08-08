983aaf3af542a507fe45f9430dde53c6b36b4430
junha <goodjunha@gmail.com>
2022-08-08 14:42:48 +0900

    ADD:: fix error

--- javaStudy/src/gugudan.java
+++ javaStudy/src/gugudan.java
@@ -4,7 +4,7 @@ public class gugudan {
         for (int i = 2; i <= 9; i++) {
             System.out.println("== " + i + "단" + " ==");
             for(int j = 1; j <= 9; j++){
-                System.out.println(i + " + " + j + " = " + i*j);
+                System.out.println(i + " * " + j + " = " + i*j);
             }
         }
     }
