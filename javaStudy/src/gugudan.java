a11801c8f268d6d4f6c61c3977bae841eae6b165
junha <goodjunha@gmail.com>
2022-08-08 14:42:00 +0900

    ADD:: ==Xdan==

--- javaStudy/src/gugudan.java
+++ javaStudy/src/gugudan.java
@@ -2,7 +2,7 @@ public class gugudan {
     public static void main(String[] args) {
 
         for (int i = 2; i <= 9; i++) {
-            System.out.println(i + "단");
+            System.out.println("== " + i + "단" + " ==");
             for(int j = 1; j <= 9; j++){
                 System.out.println(i + " + " + j + " = " + i*j);
             }
