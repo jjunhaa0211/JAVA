f1e96c124839343ede4efab390807f388cc007db
junha <goodjunha@gmail.com>
2022-08-10 14:28:49 +0900

    ADD:: class ArrInt3

--- javaStudy/src/arrName.java
+++ javaStudy/src/arrName.java
@@ -1,32 +1,46 @@
 public class arrName {
     public static void main(String[] args){
-        int age1 = 10;
-        int age2 = 11;
-        int age3 = 12;
 
         int[] ages = new int[3];
         ages[0] = 10;
         ages[1] = 10;
         ages[2] = 10;
 
-        String[] name = new String[3];
-        name[0] = "철수";
-        name[1] = "영희";
-        name[2] = "영수";
+//        String[] name = new String[3];
+//        name[0] = "철수";
+//        name[1] = "영희";
+//        name[2] = "영수";
 
-        chulSoo a = new chulSoo();
-        a.name = "철수";
-        a.age = 22;
-        a.stature = 180.5;
+//        chulSoo a = new chulSoo();
+//        a.name = "철수";
+//        a.age = 22;
+//        a.stature = 180.5;
+//
+//        System.out.println(a.name);
+//        System.out.println(a.age);
+//        System.out.println(a.stature);
 
-        System.out.println(a.name);
-        System.out.println(a.age);
-        System.out.println(a.stature);
+        ArrInt3 age2 = new ArrInt3();
+        var CoulSoo = age2.ChulSooAge = 10;
+        var Younghee = age2.YoungheeAge = 11;
+        var Youngsu = age2.YoungsuAge = 12;
+        age2.maritalStatus = false;
+
+        System.out.println(CoulSoo);
+        System.out.println(Younghee);
+        System.out.println(Youngsu);
     }
 }
 
-class chulSoo {
-    String name;
-    int age;
-    double stature;
+class ArrInt3 {
+    int ChulSooAge;
+    int YoungheeAge;
+    int YoungsuAge;
+    boolean maritalStatus;
 }
+
+//class chulSoo {
+//    String name;
+//    int age;
+//    double stature;
+//}
