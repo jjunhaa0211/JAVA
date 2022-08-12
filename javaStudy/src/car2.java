738d7935cd0ebcb9b77d6205349c3dd901ee0a30
junha <goodjunha@gmail.com>
2022-08-12 13:19:49 +0900

    ADD:: move Car

---
+++ javaStudy/src/car2.java
@@ -0,0 +1,10 @@
+public class car2 {
+    public static void main(String[ ] args) {
+        new car1().carRun();
+    }
+}
+class car1 {
+    void carRun() {
+        System.out.println("부릉부릉");
+    }
+}
