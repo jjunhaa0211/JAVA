d0abf8b0c00037ddcfb1a0efe61d4c55384cec4b
junha <goodjunha@gmail.com>
2022-09-24 17:39:27 +0900

    ADD:: instance

---
+++ javaStudy/src/instance.java
@@ -0,0 +1,101 @@
+//public class instance {
+//    public static void main(String[] args) {
+//        사람 person = new 사람();
+//        person.말하다();
+//    }
+//}
+//
+//class 사람 {
+//    // 사람은 `말하다` 라는 기능을 가지고 있습니다.
+//    void 말하다() {
+//        System.out.println("사람이 말합니다.");
+//    }
+//}
+
+//class instance {
+//    public static void main(String[] args) {
+//        거북이 a거북이 = new 거북이();
+//
+//        a거북이.걷다();
+//        // 출력 => 걷다
+//        a거북이.수영하다();
+//        // 출력 => 수영하다
+//    }
+//}
+//
+//class 거북이 {
+//    void 걷다(){
+//        System.out.println("거북이가 걷기 시작했다");
+//    }
+//
+//    void 수영하다() {
+//        System.out.println("거북이가 수영을 한다");
+//    }
+//}
+
+//class instance {
+//    public static void main(String[] args) {
+//        자동차 자동차a = new 자동차();
+//
+//        자동차a.달립니다();
+//    }
+//}
+//
+//class 자동차 {
+//    void 달립니다() {
+//        System.out.println("자동차가 달립니다.");
+//    }
+//}
+
+//class instance {
+//    public static void main(String[] args) {
+//        자동차 자동차1 = new 자동차();
+//        자동차1.달리다();
+//    }
+//}
+//
+//class 자동차 {
+//    void 달리다() {
+//
+//    }
+//}
+
+//class instance {
+//    public static void main(String[] args) {
+//        Main1 main1 = new Main1();
+//        main1.a();
+//        Main2 main2 = new Main2();
+//        main2.a();
+//        Main3 main3 = new Main3();
+//        main3.a();
+//        Main4 main4 = new Main4();
+//        main4.a();
+//        Main5 main5 = new Main5();
+//        main5.a();
+//
+//    }
+//}
+//class Main1 {
+//    void a() {
+//        System.out.println("Hello world!");
+//    }
+//}
+//class Main2 {
+//    void a() {
+//        System.out.println("Hello world!");
+//    }
+//}
+//class Main3 {
+//    void a() {
+//        System.out.println("Hello world!");
+//    }
+//}class Main4 {
+//    void a() {
+//        System.out.println("Hello world!");
+//    }
+//}
+//class Main5 {
+//    void a() {
+//        System.out.println("Hello world!");
+//    }
+//}
