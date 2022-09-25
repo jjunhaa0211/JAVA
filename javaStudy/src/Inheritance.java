e72c7e2b35598968bec9f8887504a0bc99b640d9
junha <goodjunha@gmail.com>
2022-09-25 17:11:46 +0900

    ADD:: Inheritance

--- javaStudy/src/Inheritance.java
+++ javaStudy/src/Inheritance.java
@@ -1,15 +1,15 @@
-//public class Inheritance {
-//    public static void main(String[] args) {
-//        사람 a사람 = new 사람();
-//        a사람.숨쉬다();
-//    }
-//}
-//class 동물 {
-//    void 숨쉬다() {
-//        System.out.println("숨쉬다.");
-//    }
-//}
-//
-//class 사람 {
-//
-//}
+public class Inheritance {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+        a사람.숨쉬다();
+    }
+}
+class 동물 {
+    void 숨쉬다() {
+        System.out.println("숨쉬다.");
+    }
+}
+
+class 사람 extends 동물 {
+
+}
