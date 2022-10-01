2ea6fc676691c094c5eb6b95928d6f91709ed80b
junha <goodjunha@gmail.com>
2022-10-01 15:50:39 +0900

    ADD:: casting(Weapon)

--- javaStudy/src/Main.java
+++ javaStudy/src/Main.java
@@ -18,27 +18,53 @@
 
 // 문제 : 아래가 실행되도록 해주세요.
 
+//class Weapon {
+//    public static void main(String[] args) {
+//        무기 a칼 = new 칼();
+//        a칼 = new 활();
+//    }
+//}
+//
+//class 무기 {
+//    void 공격(){
+//        System.out.println("공격하다");
+//    }
+//}
+//
+//class 칼 extends 무기 {
+//    void 베기(){
+//        System.out.println("베다");
+//    }
+//}
+//
+//class 활 extends 무기 {
+//    void 쏘기(){
+//        System.out.println("쏘기");
+//    }
+//}
+
 class Weapon {
     public static void main(String[] args) {
-        무기 a칼 = new 칼();
-        a칼 = new 활();
+        무기 a무기 = new 칼();
+        a무기.공격();
+        // 출력 : 칼로 공격합니다.
     }
 }
 
 class 무기 {
-    void 공격(){
-        System.out.println("공격하다");
+    void 공격() {
+
     }
 }
 
-class 칼 extends 무기 {
-    void 베기(){
-        System.out.println("베다");
+class 칼 extends 무기{
+    void 공격() {
+        System.out.println("칼로 찌르다!!!");
     }
 }
 
 class 활 extends 무기 {
-    void 쏘기(){
-        System.out.println("쏘기");
+    void 공격() {
+        System.out.println("활로 쏘다!!!");
     }
 }
