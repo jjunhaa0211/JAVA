b95cd13b124d958e6969e52a89c737f07aaac51c
junha <goodjunha@gmail.com>
2022-10-01 15:59:48 +0900

    ADD:: 매개 변수를 사용하여 싸우기

--- javaStudy/src/Main.java
+++ javaStudy/src/Main.java
@@ -43,28 +43,51 @@
 //    }
 //}
 
+//class Weapon {
+//    public static void main(String[] args) {
+//        무기 a무기 = new 칼();
+//        a무기.공격();
+//        // 출력 : 칼로 공격합니다.
+//    }
+//}
+//
+//class 무기 {
+//    void 공격() {
+//
+//    }
+//}
+//
+//class 칼 extends 무기{
+//    void 공격() {
+//        System.out.println("칼로 찌르다!!!");
+//    }
+//}
+//
+//class 활 extends 무기 {
+//    void 공격() {
+//        System.out.println("활로 쏘다!!!");
+//    }
+//}
+
 class Weapon {
     public static void main(String[] args) {
-        무기 a무기 = new 칼();
-        a무기.공격();
-        // 출력 : 칼로 공격합니다.
+        전사 a전사 = new 전사();
+
+        a전사.공격("브러이언");
+        // 브라이언이(가) 칼(으)로 공격합니다.
+
+        a전사.공격("필립이");
+        // 필립이(가) 창(으)로 공격합니다.
+
+        a전사.공격("마크");
+        // 마크(가) 지팡이(으)로 공격합니다.
     }
 }
 
-class 무기 {
-    void 공격() {
-
+class 전사 {
+    void 공격(String a) {
+        System.out.println(a + "(가) 창(으)로 공격합니다.");
     }
 }
 
-class 칼 extends 무기{
-    void 공격() {
-        System.out.println("칼로 찌르다!!!");
-    }
-}
 
-class 활 extends 무기 {
-    void 공격() {
-        System.out.println("활로 쏘다!!!");
-    }
-}
